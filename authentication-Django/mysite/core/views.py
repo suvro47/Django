22d527caba97cd4073e0a54690d50b2c191b5770
Sohel Suvro <suvro047@gmail.com>
2019-11-27 15:24:33 +0600

    new

---
+++ authentication-Django/mysite/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
