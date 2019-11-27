1cae075ea29e55142620a9cff1630a2cbc4d0fb2
Sohel Suvro <suvro047@gmail.com>
2019-11-27 19:39:19 +0600

    new

--- authenticatoin- HTML Form/online_judge/bob/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-
-def home(request):
-    """
-    url: http://127.0.0.1:8000/bob/home/
-    """
-    return render(request, 'home.html')
