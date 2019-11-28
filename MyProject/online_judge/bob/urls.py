bc6f628a382ebbcfc5f92879a6300c815d0476a4
Sohel Suvro <suvro047@gmail.com>
2019-11-28 15:07:15 +0600

    online_judge

--- MyProject/online_judge/bob/urls.py
+++ MyProject/online_judge/bob/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user-list/', views.user_list, name='user-list'),
+    path('single-user/<user_id>/', views.single_user, name='single-user'),
 ]
--- authentication- Django Form - II/mysite/core/views.py
+++ authentication- Django Form - II/mysite/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 from .forms import RegistrationForm
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def register(request):
@@ -16,3 +21,14 @@ def register(request):
 
 
 #  no need to define login and logout views
+
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'all_user_list': users})
+
+
+def single_user(request, user_id):
+    user = User.objects.get(pk=user_id)
+    return render(request, 'single_user.html', {'user': user})
+
