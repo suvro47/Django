631c167fcf01ceae827ddd02274b5fbefa73fcd6
Sohel Suvro <suvro047@gmail.com>
2020-01-18 01:24:53 +0600

    basic

--- InformationSystem/CSEinfo/Dept/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Student
-from .models import Teacher
-from .models import Staff
-
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Staff)
